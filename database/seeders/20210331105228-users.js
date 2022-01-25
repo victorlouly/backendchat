531e2dea4b41ddffefa9870a5fb2fa8c696afc14
Victor Hugo Silva <victor.silva@kidsbrasil.local>
2022-01-25 09:32:17 -0300

    first commit

---
+++ database/seeders/20210331105228-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const bcrypt = require('bcrypt')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'Victor',
+        email: 'teste@gmail.com',
+        password: bcrypt.hashSync('secret', 10),
+        gender: 'male'
+      },
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
